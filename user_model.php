d357069f84dc9a756b0da55d3011c3441119eb30
Md.Enamul Haq <35990207+EnamulHaq@users.noreply.github.com>
2019-01-23 12:30:32 +0600

    models/user_model.php
    Create user_model.php in model folder

---
+++ user_model.php
@@ -0,0 +1,29 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+class User_model extends CI_Model
+{
+
+	public function __construct() 
+     {
+           parent::__construct(); 
+           $this->load->database();
+     }
+
+    
+    /**
+     * This function is used to add new class to system
+     * @return number $insert_id : This is last inserted id
+     */
+    function add($data)
+    {
+        
+        $this->db->insert('user', $data);
+        $insert_id = $this->db->insert_id();
+        return $insert_id;
+    }
+
+
+
+}
+
+  
